91cc15fbb766dd0f15cee9befb14eaa7b31049d3
Robert Metcalf <rmetcalf9@googlemail.com>
2019-01-06 14:34:13 +0000

    Adding scaffolded store

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,8 +1,25 @@
 
+/*
+ * commonPreLoad called before each page load
+ * We need to load the auth providers and display invalid config if it failed
+ */
+function commonPreLoad (to, from, next) {
+//   if ((globalStore.getters.datastoreState === 'LOGGED_IN') || (globalStore.getters.datastoreState === 'LOGGED_IN_SERVERDATA_LOADED')) {
+//     next()
+//     return
+//   }
+//   next({
+//     path: '/login',
+//     query: { redirect: to.fullPath }
+//   })
+  next()
+}
+
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
+    beforeEnter: commonPreLoad,
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
