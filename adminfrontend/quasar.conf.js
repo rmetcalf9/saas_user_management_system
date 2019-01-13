7ce1406b2dd78acbc7a5ce51dedeb9d06207ccc0
Robert Metcalf <rmetcalf9@googlemail.com>
2019-01-13 15:55:04 +0000

    Adjusting port so can work alongside main frontend

--- adminfrontend/quasar.conf.js
+++ adminfrontend/quasar.conf.js
@@ -35,7 +35,7 @@ module.exports = function (ctx) {
     },
     devServer: {
       // https: true,
-      // port: 8080,
+      port: 8092,
       open: true // opens browser window automatically
     },
     // framework: 'all' --- includes everything; for dev only!
