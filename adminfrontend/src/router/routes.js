249f28fed44c475fe127092875aa59f4c9387e70
Robert Metcalf <rmetcalf9@googlemail.com>
2019-01-15 10:01:17 +0000

    Hand off to login page

--- adminfrontend/src/router/routes.js
+++ adminfrontend/src/router/routes.js
@@ -5,7 +5,8 @@ function getAlteredHost (origHost, hostLookupList) {
       return hostLookupList[x].b
     }
   }
-  return origHost
+  console.log('Failed to lookup ' + origHost + ' - don\'t know how to login')
+  return 'UNKNOWN'
 }
 
 function checkLoginNeeded (to, from, next) {
@@ -20,15 +21,13 @@ function checkLoginNeeded (to, from, next) {
     next()
     return
   }
-  console.log('TODO work out where to forward to')
   // ##console.log(this.$route.query.page)
   // ##console.log(to.query)
 
   // https://xx.com:123/a/b/c/vx/public/web/frontend/#/***
   // https://xx.com:123/a/b/c/vx/public/web/adminfrontend/#/***
 
-  //
-  var returnAddress = window.location.href
+  // window.location.href contains old path if we are redirecting
 
   // http:
   // console.log(window.location.protocol)
@@ -45,26 +44,37 @@ function checkLoginNeeded (to, from, next) {
   // #/usersystem/selectAuth
   // console.log(window.location.hash)
 
+  var thisQuasarPath = to.path
+  if (thisQuasarPath.startsWith('/')) {
+    thisQuasarPath = thisQuasarPath.substr(1)
+  }
+  thisQuasarPath = '#/' + thisQuasarPath
+  console.log(thisQuasarPath)
   var locationToGoTo = ''
   if (window.location.pathname.includes('/public/web/adminfrontend/')) {
-    locationToGoTo = window.location.protocol + '//' + window.location.host + window.location.pathname.replace('/public/web/adminfrontend/', '/public/web/frontend/') + window.location.hash
+    locationToGoTo = window.location.protocol + '//' + window.location.host + window.location.pathname.replace('/public/web/adminfrontend/', '/public/web/frontend/') + thisQuasarPath
   } else {
     var hostLookup = [
       {a: 'localhost:8082', b: 'localhost:8081'},
       {a: 'cat-sdts.metcarob-home.com:8082', b: 'cat-sdts.metcarob-home.com:8081'},
-      {a: 'somefunnyhostname.com:8082', b: 'somefunnyhostname.com:8081'}
+      {a: 'somefunnyhostname.com:5082', b: 'somefunnyhostname.com:5081'}
     ]
-    locationToGoTo = window.location.protocol + '//' + getAlteredHost(window.location.host, hostLookup) + window.location.pathname + window.location.hash
+    locationToGoTo = window.location.protocol + '//' + getAlteredHost(window.location.host, hostLookup) + window.location.pathname + thisQuasarPath
   }
+  var returnAddress = window.location.protocol + '//' + window.location.host + window.location.pathname + thisQuasarPath
 
-  console.log('GOTO:' + locationToGoTo)
+  window.location.href = locationToGoTo
+  // console.log('GOTO:' + locationToGoTo)
   console.log('RET:' + returnAddress)
   next()
 }
 
 const routes = [
   {
-    path: '/',
+    path: '/', redirect: '/usersystem/'
+  },
+  {
+    path: '/:tenantName',
     component: () => import('layouts/MyLayout.vue'),
     beforeEnter: checkLoginNeeded,
     children: [
