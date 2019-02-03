eeb70869e9242385ea40b0c897447a6c6557b774
Robert Metcalf <rmetcalf9@googlemail.com>
2019-02-03 18:53:04 +0000

    Refactoring to allow two ways to redirect to login page

--- adminfrontend/src/router/routes.js
+++ adminfrontend/src/router/routes.js
@@ -11,6 +11,26 @@ function getAlteredHost (origHost, hostLookupList) {
   return 'UNKNOWN'
 }
 
+function moveToLoginService (thisQuasarPath) {
+  var quasarPathForTenenat = '#/' + thisQuasarPath.substr(0, thisQuasarPath.indexOf('/'))
+  thisQuasarPath = '#/' + thisQuasarPath
+
+  var locationToGoTo = ''
+  if (window.location.pathname.includes('/public/web/adminfrontend/')) {
+    locationToGoTo = window.location.protocol + '//' + window.location.host + window.location.pathname.replace('/public/web/adminfrontend/', '/public/web/frontend/') + quasarPathForTenenat
+  } else {
+    var hostLookup = [
+      {a: 'localhost:8082', b: 'localhost:8081'},
+      {a: 'cat-sdts.metcarob-home.com:8082', b: 'cat-sdts.metcarob-home.com:8081'},
+      {a: 'somefunnyhostname.com:5082', b: 'somefunnyhostname.com:5081'}
+    ]
+    locationToGoTo = window.location.protocol + '//' + getAlteredHost(window.location.host, hostLookup) + window.location.pathname + quasarPathForTenenat
+  }
+  var returnAddress = window.location.protocol + '//' + window.location.host + window.location.pathname + thisQuasarPath
+
+  window.location.href = locationToGoTo + '?usersystem_returnaddress=' + encodeURIComponent(returnAddress)
+}
+
 function checkLoginNeeded (to, from, next) {
   stores().commit('globalDataStore/updateTenantName', to.params.tenantName)
 
@@ -27,6 +47,7 @@ function checkLoginNeeded (to, from, next) {
     next()
     return
   }
+
   var thisQuasarPath = to.path
   var logoutClickCurRoute = stores().state.globalDataStore.logoutClickCurRoute
   if (typeof (logoutClickCurRoute) !== 'undefined') {
@@ -37,50 +58,9 @@ function checkLoginNeeded (to, from, next) {
   if (thisQuasarPath.startsWith('/')) {
     thisQuasarPath = thisQuasarPath.substr(1)
   }
-  var quasarPathForTenenat = '#/' + thisQuasarPath.substr(0, thisQuasarPath.indexOf('/'))
-  thisQuasarPath = '#/' + thisQuasarPath
-
   stores().commit('globalDataStore/SET_LOGOUT_CLICK_CUR_ROUTE', null)
-  // ##console.log(this.$route.query.page)
-  // ##console.log(to.query)
 
-  // https://xx.com:123/a/b/c/vx/public/web/frontend/#/***
-  // https://xx.com:123/a/b/c/vx/public/web/adminfrontend/#/***
-
-  // window.location.href contains old path if we are redirecting
-
-  // http:
-  // console.log(window.location.protocol)
-
-  // localhost:8002
-  // console.log(window.location.host)
-
-  // Path name is the bit before the hash
-  // /
-  // /a/b/c/vx/public/web/adminfrontend/
-  // console.log(window.location.pathname)
-
-  // #/
-  // #/usersystem/selectAuth
-  // console.log(window.location.hash)
-
-  var locationToGoTo = ''
-  if (window.location.pathname.includes('/public/web/adminfrontend/')) {
-    locationToGoTo = window.location.protocol + '//' + window.location.host + window.location.pathname.replace('/public/web/adminfrontend/', '/public/web/frontend/') + quasarPathForTenenat
-  } else {
-    var hostLookup = [
-      {a: 'localhost:8082', b: 'localhost:8081'},
-      {a: 'cat-sdts.metcarob-home.com:8082', b: 'cat-sdts.metcarob-home.com:8081'},
-      {a: 'somefunnyhostname.com:5082', b: 'somefunnyhostname.com:5081'}
-    ]
-    locationToGoTo = window.location.protocol + '//' + getAlteredHost(window.location.host, hostLookup) + window.location.pathname + quasarPathForTenenat
-  }
-  var returnAddress = window.location.protocol + '//' + window.location.host + window.location.pathname + thisQuasarPath
-
-  window.location.href = locationToGoTo + '?usersystem_returnaddress=' + encodeURIComponent(returnAddress)
-  // console.log('GOTO:' + locationToGoTo)
-  // console.log('RET:' + returnAddress)
-  next()
+  moveToLoginService(thisQuasarPath)
 }
 
 function beforeEnterMainIndexChildPage (to, from, next, pageTitle) {
