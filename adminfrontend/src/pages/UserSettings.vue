1a44b79298c7b7404eae6f1dc94e4d815e23f8d1
Robert Metcalf <rmetcalf9@googlemail.com>
2019-02-19 11:53:09 +0000

    Adding user settings display

--- adminfrontend/src/pages/UserSettings.vue
+++ adminfrontend/src/pages/UserSettings.vue
@@ -1,5 +1,20 @@
 <template>
   <q-page>
+    <q-list >
+      <q-item>
+        <q-item-main >
+          <q-item-tile label>User:</q-item-tile>
+          <q-item-tile sublabel>{{ loggedInUserCookie.known_as }}</q-item-tile>
+        </q-item-main>
+      </q-item>
+      <q-item>
+        <q-item-main >
+          <q-item-tile label>Roles:</q-item-tile>
+          <q-item-tile sublabel v-for="curVal in loggedInUserCookie.ThisTenantRoles" :key=curVal>{{ curVal }}</q-item-tile>
+        </q-item-main>
+      </q-item>
+    </q-list>
+
     {{ loggedInUserCookie }}
   </q-page>
 </template>
