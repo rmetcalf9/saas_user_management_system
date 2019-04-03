0a72679c5cb9b94f31cf2914b16a46b089709fc0
Robert Metcalf <rmetcalf9@googlemail.com>
2019-04-03 12:44:55 +0100

    Implemented container memory test step

--- Dockerfile
+++ Dockerfile
@@ -61,5 +61,5 @@ CMD ["/run_app_docker.sh"]
 HEALTHCHECK --interval=30s --timeout=3s \
   CMD curl -f http://127.0.0.1:80/frontend/index.html?healthcheck=true || exit 1
 
-##docker run --rm --name saas_user_management_system -e APIAPP_APIURL=${EXTURL}:${EXTPORT80}/api -e APIAPP_APIDOCSURL=${EXTURL}:${EXTPORT80}/apidocs -e APIAPP_FRONTENDURL=${EXTURL}:${EXTPORT80}/frontend -e APIAPP_MASTERPASSWORDFORPASSHASH=ABC -e APIAPP_DEFAULTHOMEADMINUSERNAME=admin -e APIAPP_DEFAULTHOMEADMINPASSWORD=admin -p 80:80 -d metcarob/saas_user_managmenet_system:latest
+##docker run --network pythontestingnetwork --rm --name saas_user_management_system -e APIAPP_APIURL=${EXTURL}:${EXTPORT80}/api -e APIAPP_APIDOCSURL=${EXTURL}:${EXTPORT80}/apidocs -e APIAPP_FRONTENDURL=${EXTURL}:${EXTPORT80}/frontend -e APIAPP_MASTERPASSWORDFORPASSHASH=ABC -e APIAPP_DEFAULTHOMEADMINUSERNAME=admin -e APIAPP_DEFAULTHOMEADMINPASSWORD=admin -p 80:80 -d metcarob/saas_user_managmenet_system:latest
 
--- services/testContainerMemory/test_main.py
+++ services/testContainerMemory/test_main.py
@@ -4,7 +4,7 @@ import requests
 import os
 import json
 
-baseURL="http://saas_user_management_system:8098"
+baseURL="http://saas_user_management_system:80"
 if ('BASEURL_TO_TEST' in os.environ):
   baseURL=os.environ['BASEURL_TO_TEST']
 
@@ -13,11 +13,11 @@ class test_containerAPI(unittest.TestCase):
 
   def test_WeCanGetToSwaggerFile(self):
     result = requests.get(baseURL + "/api/swagger.json")
-    self.assertEqual(result.status_code, 200)
+    self.assertEqual(result.status_code, 200, msg="Wrong status code recieved from " + baseURL + "/api/swagger.json")
 
   def test_ContainerVersionMatchesEnviromentVariable(self):
-    self.assertTrue('EXPECTED_CONTAINER_VERSION' in os.environ)
-    result = requests.get(baseURL + "/api/serverinfo/")
-    self.assertEqual(result.status_code, 200)
+    self.assertTrue('EXPECTED_CONTAINER_VERSION' in os.environ, msg="EXPECTED_CONTAINER_VERSION missing from enviroment")
+    result = requests.get(baseURL + "/api/public/login/serverinfo")
+    self.assertEqual(result.status_code, 200, msg="Wrong status code recieved from " + baseURL + "/api/public/login/serverinfo")
     resultJSON = json.loads(result.text)
     self.assertEqual(resultJSON['Server']['Version'], os.environ['EXPECTED_CONTAINER_VERSION'])
