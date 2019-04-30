e17729aaf7c753c08d2c1cb94030c0b65474a6e3
Robert Metcalf <rmetcalf9@googlemail.com>
2019-04-30 11:21:52 +0100

    Assembled data for security settings page

--- frontend/src/pages/SecuritySettings.vue
+++ frontend/src/pages/SecuritySettings.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="fixed-center text-center">
-    <p class="text-faded">Security Settings Page</p>
-    <q-btn
-      color="secondary"
-      style="width:200px;"
-      @click="goBackClick"
-    >Go back</q-btn>
+  <div class="fixed-center">
+    <div>
+      <div class="row">
+        <h2>Security Settings</h2>
+      </div>
+      <div class="row">
+        <div class="col">
+          <p>Your authentication methods:</p>
+        </div>
+      </div>
+      <div class="row">
+        <div v-for="curVal in currentTenantAuthsWithAuthProvData" :key=curVal.AuthUserKey>
+          {{ curVal }}
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="row">
+        <q-btn
+          color="primary"
+          style="width:200px;"
+          @click="goBackClick"
+        >Go back</q-btn>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +33,10 @@ import callbackHelper from '../callbackHelper'
 
 function getEmptyUserSettingsData () {
   return {
-    a: 'a'
+    loggedInPerson: {
+      personAuths: [
+      ]
+    }
   }
 }
 
@@ -24,6 +45,22 @@ export default {
   data () {
     return {
       UserSettingsData: getEmptyUserSettingsData()
+    }
+  },
+  computed: {
+    currentTenantAuthsWithAuthProvData () {
+      var TTT = this
+      return TTT.UserSettingsData.loggedInPerson.personAuths.filter(function (a) { return a.tenantName === TTT.$store.state.globalDataStore.tenantInfo.Name }).map(
+        function (a) {
+          return {
+            AuthUserKey: a.AuthUserKey,
+            auth: a,
+            internalAuthProv: TTT.$store.state.globalDataStore.tenantInfo.AuthProviders.filter(function (b) {
+              return b.guid === a.AuthProviderGUID
+            })[0]
+          }
+        }
+      )
     }
   },
   methods: {
