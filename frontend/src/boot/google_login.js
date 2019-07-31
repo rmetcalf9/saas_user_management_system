48fc4e5218f7480bb3b12b216b77359c00ad311c
Robert Metcalf <rjmetcal@googlemail.com>
2019-07-31 14:11:49 +0100

    Need to import for sideeffects only

--- frontend/src/boot/google_login.js
+++ frontend/src/boot/google_login.js
@@ -1,10 +1,10 @@
 // import something here
 // var gapi = require('gapi')
-import gapi from './googleclient'
+import './googleclient'
 // https://www.npmjs.com/package/gapi
 
 // leave the export, even if you don't use it
 export default ({ app, router, Vue }) => {
   // something to do
-  Vue.prototype.$gapi = gapi
+  Vue.prototype.$gapi = window.gapi
 }
