33320c331a6f376802ee7bb209dfef9b8bc6859f
Robert Metcalf <rmetcalf9@googlemail.com>
2022-06-28 10:12:56 +0100

    fix switch to pytest

--- services/continous_test.sh
+++ services/continous_test.sh
@@ -10,11 +10,11 @@ echo 'To test only highlighted with @wipd'
 echo 'e.g. sudo ./continous_test.sh wip'
 
 if [ $# -eq 0 ]; then
-  until ack -f --python  ./src ./test | entr -d nosetests --rednose; do sleep 1; done
+  until ack -f --python  ./src ./test | entr -d python3 -m pytest ./test; do sleep 1; done
 else
   if [ $# -eq 1 ]; then
     echo "Testing ${1}"
-    until ack -f --python  ./src ./test | entr -d nosetests -a ${1} --rednose; do sleep 1; done
+    until ack -f --python  ./src ./test | entr -d python3 -m pytest -a ${1} ./test; do sleep 1; done
   else
     echo "Testing ${1} with verbose option (Single Run)"
     nosetests -v -a ${1} --rednose
