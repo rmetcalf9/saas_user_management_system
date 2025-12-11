47d73eb633de27d0b75104affda208a89236e55a
Robert Metcalf <rmetcalf9@googlemail.com>
2025-12-11 10:14:03 +0000

    Full deploy added to action

--- _repo_vars.sh
+++ _repo_vars.sh
@@ -9,6 +9,13 @@ export RJM_MAJOR_VERSION=$(echo ${RJM_VERSION%%.*})
 export RJM_DOCKER_SERVICE_NAME=${PROJECT_NAME}_${RJM_VERSION_UNDERSCORE}
 export QUASARBUILDIMAGE="metcarob/docker-build-quasar-app:0.0.12"
 export QUASARBUILDIMAGE_ADMINFRONTEND="metcarob/docker-build-quasar-app:0.0.33"
+export RJM_DOCKERWSCALLER_IMAGE="metcarob/docker-ws-caller:0.7.19"
+export RJM_DOCKER_KONG_API_URL="http://tasks.kong:8001"
+export RJM_DOCKER_SERVICE_URL=tasks.${RJM_DOCKER_SERVICE_NAME}
+export JWT_COOKIE_NAME="jwt-auth-token"
+export AUTHED_ACL_WHITELIST="saas_user_management"
+export AUTHED_ACL_BLACKLIST=""
+export RJM_KONG_UPSTREAM_NAME=${PROJECT_NAME}_${RJM_MAJOR_VERSION}
 
 export RJM_PYTHON_TEST_IMAGE=python:3.10
 
