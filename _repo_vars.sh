1817db2f7d43ea4b129d13bea513c7da32001fd2
Robert Metcalf <rmetcalf9@googlemail.com>
2026-04-13 19:04:23 +0100

    Switching frontend build to new version

--- _repo_vars.sh
+++ _repo_vars.sh
@@ -8,7 +8,7 @@ export RJM_VERSION=$(cat ./VERSION)
 export RJM_VERSION_UNDERSCORE=$(cat ./VERSION | tr '.' '_')
 export RJM_MAJOR_VERSION=$(echo ${RJM_VERSION%%.*})
 export RJM_DOCKER_SERVICE_NAME=${PROJECT_NAME}_${RJM_VERSION_UNDERSCORE}
-export QUASARBUILDIMAGE="metcarob/docker-build-quasar-app:0.0.12"
+export QUASARBUILDIMAGE="metcarob/docker-build-quasar-app:0.0.33"
 export QUASARBUILDIMAGE_ADMINFRONTEND="metcarob/docker-build-quasar-app:0.0.33"
 export RJM_DOCKERWSCALLER_IMAGE="metcarob/docker-ws-caller:0.7.19"
 export RJM_DOCKER_KONG_API_URL="http://tasks.kong:8001"
@@ -25,4 +25,3 @@ export DOCKER_USERNAME=metcarob
 export RJM_USERMANAGEMENT_CONTAINER="metcarob/saas_user_management:0.0.224_rootver"
 
 export VAULT_DEPLOYMENT_APPROLE_SECRET_LOCATION=/deploymentapproles/${PROJECT_NAME}
-
