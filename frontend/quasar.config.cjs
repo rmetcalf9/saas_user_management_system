177a8b5a839b6b5e09182ff89f93d6183410146b
Robert Metcalf <rmetcalf9@googlemail.com>
2026-05-11 21:09:52 +0100

    Removing service worker

--- frontend/quasar.config.cjs
+++ frontend/quasar.config.cjs
@@ -160,7 +160,7 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-webpack/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: 'Disabled', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
