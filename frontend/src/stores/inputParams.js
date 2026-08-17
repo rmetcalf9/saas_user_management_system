e26561455369368dba545f59ce8a492f4c2bd73e
Robert <rmetcalf9@googlemail.com>
2026-08-17 15:16:24 +0100

    Login will retain return url even if refreshed

--- frontend/src/stores/inputParams.js
+++ frontend/src/stores/inputParams.js
@@ -1,17 +1,41 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
 
+const SESSION_STORAGE_KEY = 'inputParams'
+
 // This store remembers the input params sent by the app calling the login
 
 export const useInputParamsStore = defineStore('inputParamsStore', {
-  state: () => ({
-    usersystemReturnaddress: '',
-    usersystemMessage: ''
-  }),
-  getters: {},
+  state: () => {
+    let storedParams = {}
+
+    try {
+      storedParams = JSON.parse(
+        sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}'
+      )
+    } catch (e) {
+      console.log('RJMDEBUG inputParamsStore invalid session storage')
+    }
+
+    return {
+      usersystemReturnaddress: storedParams.usersystemReturnaddress || '',
+      usersystemMessage: storedParams.usersystemMessage || ''
+    }
+  },
+  getters: {
+    getUsersystemReturnaddress: (state) => state.usersystemReturnaddress,
+    getUsersystemMessage: (state) => state.usersystemMessage
+  },
   actions: {
     setParams ({ usersystemReturnaddress, usersystemMessage }) {
       this.usersystemReturnaddress = usersystemReturnaddress
       this.usersystemMessage = usersystemMessage
+      sessionStorage.setItem(
+        SESSION_STORAGE_KEY,
+        JSON.stringify({
+          usersystemReturnaddress,
+          usersystemMessage
+        })
+      )
     },
     clearUsersystemMessage () {
       this.usersystemMessage = ''
